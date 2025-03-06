1a5c73bd4da760b2d8526aabe836502ff3279a23
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-06 21:01:51 +0000

    Create theme_toggle.js

---
+++ web/static/js/theme_toggle.js
@@ -0,0 +1,7 @@
+// Theme toggle functionality
+function toggleDarkMode() {
+    const toggleButton = document.getElementById('toggle_dark_mode_button');
+    if (toggleButton) {
+        toggleButton.click();
+    }
+} 
