34623ec404c34be31a9d2c3afc4720756f39a36c
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 09:51:15 +0000

    Create __init__.py

---
+++ web/ui/views/drawing/__init__.py
@@ -0,0 +1,9 @@
+# MNIST Digit Classifier
+# Copyright (c) 2025
+# File: ui/views/drawing/__init__.py
+# Description: Drawing view initialization
+# Created: 2024-05-01
+
+from ui.views.drawing.drawing_view import DrawingView
+
+# Import view-specific components here
