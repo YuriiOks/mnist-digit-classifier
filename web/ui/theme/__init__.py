8a0eaf87d2dcdad82a4d520f20c486e4627ea81e
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 09:51:38 +0000

    Create __init__.py

---
+++ web/ui/theme/__init__.py
@@ -0,0 +1,7 @@
+# MNIST Digit Classifier
+# Copyright (c) 2025
+# File: ui/theme/__init__.py
+# Description: Theme management initialization
+# Created: 2024-05-01
+
+from ui.theme.theme_manager import ThemeManager 
