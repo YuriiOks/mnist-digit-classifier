baf58ed42eb53b6fefe2d2acac43988d8dfd5638
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 09:50:53 +0000

    Create __init__.py

---
+++ web/ui/views/settings/__init__.py
@@ -0,0 +1,10 @@
+# MNIST Digit Classifier
+# Copyright (c) 2025
+# File: ui/views/settings/__init__.py
+# Description: Init file for settings views package
+# Created: 2024-05-01
+
+# Import views for easier access
+from ui.views.settings.font_preview import FontPreviewView
+
+__all__ = ['FontPreviewView']
