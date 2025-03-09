3703a62279b86c5cb7411352c14caf21faae5982
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 09:51:50 +0000

    Create __init__.py

---
+++ web/ui/components/controls/__init__.py
@@ -0,0 +1,8 @@
+# MNIST Digit Classifier
+# Copyright (c) 2025
+# File: ui/components/controls/__init__.py
+# Description: Control components initialization
+# Created: 2024-05-01
+
+from ui.components.controls.buttons import Button, PrimaryButton, SecondaryButton, IconButton
+from ui.components.controls.toggles import Toggle, ThemeToggle
