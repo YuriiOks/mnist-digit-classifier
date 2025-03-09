d5099de9223d72e94f44ef4265679209e7e5e200
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 09:51:55 +0000

    Create __init__.py

---
+++ web/ui/components/__init__.py
@@ -0,0 +1,8 @@
+# MNIST Digit Classifier
+# Copyright (c) 2025
+# File: ui/components/cards/__init__.py
+# Description: Card components initialization
+# Created: 2024-05-01
+
+from ui.components.cards.card import Card
+from ui.components.cards.content_card import ContentCard
