b30d35c7fe74a7128a8ff8305c13709e5a7d45ec
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 20:03:54 +0000

    Update __init__.py

--- web/ui/components/base/__init__.py
+++ web/ui/components/base/__init__.py
@@ -1,7 +1,11 @@
 # MNIST Digit Classifier
 # Copyright (c) 2025
 # File: ui/components/base/__init__.py
-# Description: Base component package initialization
+# Description: Base components package
 # Created: 2024-05-01
 
-from ui.components.base.component import Component 
+"""Base components for the MNIST Digit Classifier application."""
+
+from ui.components.base.component import Component
+
+__all__ = ['Component'] 
