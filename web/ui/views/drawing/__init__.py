0a1554d825ffb1fa6545262dd7a6bdd9af133e5f
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 20:03:27 +0000

    Update __init__.py

--- web/ui/views/drawing/__init__.py
+++ web/ui/views/drawing/__init__.py
@@ -1,9 +1,11 @@
 # MNIST Digit Classifier
 # Copyright (c) 2025
 # File: ui/views/drawing/__init__.py
-# Description: Drawing view initialization
+# Description: Drawing view package initialization
 # Created: 2024-05-01
 
 from ui.views.drawing.drawing_view import DrawingView
+
+__all__ = ['DrawingView']
 
 # Import view-specific components here
