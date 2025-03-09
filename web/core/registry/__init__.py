cf7f8197c63c931bfa0b3c63e7ba67a3da3e7661
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 09:57:17 +0000

    Create __init__.py

---
+++ web/core/registry/__init__.py
@@ -0,0 +1,7 @@
+# MNIST Digit Classifier
+# Copyright (c) 2025
+# File: core/registry/__init__.py
+# Description: Registry package initialization
+# Created: 2024-05-01
+
+# Make registry a proper Python package 
