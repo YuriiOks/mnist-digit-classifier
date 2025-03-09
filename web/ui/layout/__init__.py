8b5c6fae47596aa8390a734dd1202be35835ca8d
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 09:51:40 +0000

    Create __init__.py

---
+++ web/ui/layout/__init__.py
@@ -0,0 +1,9 @@
+# MNIST Digit Classifier
+# Copyright (c) 2025
+# File: ui/layout/__init__.py
+# Description: Layout components package initialization
+# Created: 2024-05-01
+
+from ui.layout.header import Header
+from ui.layout.footer import Footer
+from ui.layout.sidebar import Sidebar 
