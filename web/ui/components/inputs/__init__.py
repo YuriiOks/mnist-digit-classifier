e487d0578960f88ec99d68545884abf7cc7f3848
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 09:51:48 +0000

    Create __init__.py

---
+++ web/ui/components/inputs/__init__.py
@@ -0,0 +1,7 @@
+# MNIST Digit Classifier
+# Copyright (c) 2025
+# File: ui/components/inputs/__init__.py
+# Description: Input components initialization
+# Created: 2024-05-01
+
+from ui.components.inputs.text_input import TextInput
