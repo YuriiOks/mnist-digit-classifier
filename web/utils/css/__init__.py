b5fff5ed09151ac270bf9471b7e3d20cf68be91a
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 09:50:46 +0000

    Create __init__.py

---
+++ web/utils/css/__init__.py
@@ -0,0 +1,10 @@
+# MNIST Digit Classifier
+# Copyright (c) 2025
+# File: utils/css/__init__.py
+# Description: CSS utilities package for the MNIST Digit Classifier
+# Created: 2024-05-01 
+
+# Import key functions to make them available at the package level
+from utils.css.css_loader import load_css_file, inject_css, load_and_inject_css, load_theme_css
+
+# This makes the css directory a proper Python package
