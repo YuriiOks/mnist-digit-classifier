3ce53fdea03a459108cb8246033b2a2e8ee3e8e5
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 09:50:30 +0000

    Create __init__.py

---
+++ web/utils/js/__init__.py
@@ -0,0 +1,8 @@
+# MNIST Digit Classifier
+# Copyright (c) 2025
+# File: utils/js/__init__.py
+# Description: JavaScript utilities package for the MNIST Digit Classifier
+# Created: 2024-05-01 
+
+# Import key functions to make them available at the package level
+from utils.js.js_loader import load_js_file, inject_js, load_and_inject_js, create_callback_js
