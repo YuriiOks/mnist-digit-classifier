054cfa2190505cdceaae5b9293a74ee0d13076b1
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 09:50:41 +0000

    Create __init__.py

---
+++ web/utils/html/__init__.py
@@ -0,0 +1,9 @@
+# MNIST Digit Classifier
+# Copyright (c) 2025
+# File: utils/html/__init__.py
+# Description: HTML utilities package for the MNIST Digit Classifier
+# Created: 2024-05-01 
+
+# Import key functions to make them available at the package level
+from utils.html.html_loader import load_html_template, load_component_template, load_view_template
+from utils.html.template_engine import TemplateEngine
