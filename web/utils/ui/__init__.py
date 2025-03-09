b2c3027225cc9521c0cf2e6356e236c38b16738c
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 09:50:36 +0000

    Create __init__.py

---
+++ web/utils/ui/__init__.py
@@ -0,0 +1,13 @@
+# MNIST Digit Classifier
+# Copyright (c) 2025
+# File: utils/ui/__init__.py
+# Description: UI utilities package
+# Created: 2024-05-01
+
+"""UI utilities for the MNIST Digit Classifier application."""
+
+# Import modules for easier access - but avoid circular imports
+# from utils.ui.layout import apply_standard_layout, fix_layout_issues
+
+# __all__ will be populated by the imported modules
+__all__ = [] 
