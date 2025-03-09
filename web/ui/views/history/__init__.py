69c9f14f7d5ee51cc6ef27f9eacf9aeaee1cd252
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 09:51:33 +0000

    Create __init__.py

---
+++ web/ui/views/history/__init__.py
@@ -0,0 +1,9 @@
+# MNIST Digit Classifier
+# Copyright (c) 2025
+# File: ui/views/history/__init__.py
+# Description: History view initialization
+# Created: 2024-05-01
+
+from ui.views.history.history_view import HistoryView
+
+# Import view-specific components here
