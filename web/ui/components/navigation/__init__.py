2c0cc6b67c9f41d0f4882e17be5c0283041be51c
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-09 09:51:45 +0000

    Create __init__.py

---
+++ web/ui/components/navigation/__init__.py
@@ -0,0 +1,8 @@
+# MNIST Digit Classifier
+# Copyright (c) 2025
+# File: ui/components/navigation/__init__.py
+# Description: Navigation components initialization
+# Created: 2024-05-01
+
+from ui.components.navigation.tabs import Tabs, Tab
+from ui.components.navigation.menu import Menu, MenuItem
