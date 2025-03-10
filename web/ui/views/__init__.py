22a4f5a6188a2310d6f60d0fead1b94c43f57685
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-10 19:50:28 +0000

    Update __init__.py

--- web/ui/views/__init__.py
+++ web/ui/views/__init__.py
@@ -5,15 +5,17 @@
 # Created: 2024-05-01
 
 # Import views for easier access
-from ui.views.home import HomeView
-from ui.views.drawing import DrawingView
-from ui.views.history import HistoryView
+from ui.views.home.home_view import HomeView
+from ui.views.draw.draw_view import DrawView
+from ui.views.history.history_view import HistoryView
+from ui.views.settings.settings_view import SettingsView
 
 # Don't import subpackages here to avoid circular imports
 # These will be imported where needed directly
 
 __all__ = [
     'HomeView',
-    'DrawingView',
-    'HistoryView'
+    'DrawView',
+    'HistoryView',
+    'SettingsView'
 ] 
