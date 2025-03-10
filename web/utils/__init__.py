8ff46c26d0f62aa7cc7dc1013cf8045c7d5f1498
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-10 19:50:21 +0000

    Update __init__.py

--- web/utils/__init__.py
+++ web/utils/__init__.py
@@ -9,3 +9,24 @@
 # Import key functions to make them available at the package level
 from utils.file.path_utils import get_project_root, resolve_path, get_asset_path, get_template_path
 from utils.file.file_loader import load_text_file, load_binary_file, load_json_file, file_exists
+from utils.css.css_loader import load_css, load_theme_css, inject_css, load_css_file
+from utils.html.html_loader import load_html_template
+from utils.ui.layout import fix_layout_issues
+
+
+__all__ = [
+    "get_project_root",
+    "resolve_path",
+    "get_asset_path",
+    "get_template_path",
+    "load_text_file",
+    "load_binary_file",
+    "load_json_file",
+    "file_exists",
+    "load_css",
+    "load_theme_css",
+    "inject_css",
+    "load_css_file",
+    "load_html_template",
+    "fix_layout_issues"
+]
