de8b9726c94b26750a9cdd83d2c1d1ddaf65aa76
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-10 19:50:21 +0000

    Update __init__.py

--- web/utils/css/__init__.py
+++ web/utils/css/__init__.py
@@ -7,4 +7,11 @@
 # Import key functions to make them available at the package level
 from utils.css.css_loader import load_css_file, inject_css, load_and_inject_css, load_theme_css
 
+__all__ = [
+    "load_css_file",
+    "inject_css",
+    "load_and_inject_css",
+    "load_theme_css"
+]
+
 # This makes the css directory a proper Python package
