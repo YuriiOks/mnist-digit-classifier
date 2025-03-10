111a417edf4805faedfba9eecfd2eb3e2c0b2208
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-10 19:50:42 +0000

    Update __init__.py

--- web/ui/components/__init__.py
+++ web/ui/components/__init__.py
@@ -1,8 +1,11 @@
 # MNIST Digit Classifier
 # Copyright (c) 2025
-# File: ui/components/cards/__init__.py
-# Description: Card components initialization
+# File: ui/components/__init__.py
+# Description: UI components package initialization
 # Created: 2024-05-01
+
+# Import core components for easier access
+from ui.components.base_component import BaseComponent
 
 from ui.components.cards.card import Card
 from ui.components.cards.content_card import ContentCard
