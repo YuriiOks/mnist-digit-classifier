37ab56101fe127cd54d3bf296d9db5253d230b54
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-10 21:20:04 +0000

    Update __init__.py

--- web/utils/__init__.py
+++ web/utils/__init__.py
@@ -1,7 +1,7 @@
 # MNIST Digit Classifier
 # Copyright (c) 2025
 # File: utils/__init__.py
-# Description: Init file for utils package
+# Description: Utils package initialization
 # Created: 2024-05-01
 
 # This makes the utils directory a proper Python package
@@ -12,9 +12,14 @@ from utils.file.file_loader import load_text_file, load_binary_file, load_json_f
 from utils.css.css_loader import load_css, load_theme_css, inject_css, load_css_file
 from utils.html.html_loader import load_html_template
 from utils.ui.layout import fix_layout_issues
+from utils.ui.view_utils import apply_view_styling
+from utils.template_loader import TemplateLoader
+from utils.data_loader import DataLoader
+
 
 
 __all__ = [
+    "apply_view_styling",
     "get_project_root",
     "resolve_path",
     "get_asset_path",
@@ -28,5 +33,7 @@ __all__ = [
     "inject_css",
     "load_css_file",
     "load_html_template",
-    "fix_layout_issues"
+    "fix_layout_issues",
+    "TemplateLoader",
+    "DataLoader"
 ]
