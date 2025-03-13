adaaa33b1fdf389f2d023ba10a22fb94b465c710
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-13 17:17:40 +0000

    Update __init__.py

--- web/core/__init__.py
+++ web/core/__init__.py
@@ -2,6 +2,20 @@
 # Copyright (c) 2025
 # File: core/__init__.py
 # Description: Core package initialization
-# Created: 2024-05-01
+# Created: 2024-05-05
 
-# Make core a proper Python package
+"""Core functionality for the MNIST Digit Classifier."""
+
+from core.app_state.session_state import SessionState
+from core.app_state.theme_state import ThemeState
+from core.app_state.navigation_state import NavigationState
+from core.registry.view_registry import ViewRegistry
+from core.errors.error_handler import ErrorHandler
+
+__all__ = [
+    "SessionState",
+    "ThemeState",
+    "NavigationState",
+    "ViewRegistry",
+    "ErrorHandler"
+]
