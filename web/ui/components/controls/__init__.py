dd047fcabc2b511342dc2d5e3f04344e21a79335
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-16 14:12:59 +0000

    Update __init__.py

--- web/ui/components/controls/__init__.py
+++ web/ui/components/controls/__init__.py
@@ -1,15 +1,18 @@
 # MNIST Digit Classifier
 # Copyright (c) 2025
 # File: ui/components/controls/__init__.py
-# Description: Controls components package initialization
-# Created: 2024-05-01
+# Description: Controls package initialization
+# Created: 2025-03-16
 
-# Import control components for easier access
-# from ui.components.controls.theme_toggle import ThemeToggle  # Remove old toggle
-from ui.components.controls.bb8_toggle import BB8Toggle  # Add new toggle
+"""Control components for the MNIST Digit Classifier."""
 
+from ui.components.controls.buttons import Button, PrimaryButton, SecondaryButton, IconButton
+from ui.components.controls.bb8_toggle import BB8Toggle
 
 __all__ = [
-    # "ThemeToggle",  # Remove old toggle
-    "BB8Toggle"  # Add new toggle
+    "Button",
+    "PrimaryButton",
+    "SecondaryButton",
+    "IconButton",
+    "BB8Toggle"
 ]
