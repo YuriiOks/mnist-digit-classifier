86c61e0b4168a306a01460fc3ba4e3c6ee39c79c
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-16 20:58:27 +0000

    Delete __init__.py

--- web/ui/views/home/__init__.py
+++
@@ -1,9 +0,0 @@
-# MNIST Digit Classifier
-# Copyright (c) 2025
-# File: ui/views/home/__init__.py
-# Description: Home view initialization
-# Created: 2024-05-01
-
-# Import view-specific components here
-
-from ui.views.home.home_view import HomeView
