acc31ee7183ad68eb64dbe1af486f7962155626e
YuriiOks <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-17 14:36:42 +0000

    Update __init__.py

--- web/ui/components/controls/__init__.py
+++ web/ui/components/controls/__init__.py
@@ -2,17 +2,21 @@
 # Copyright (c) 2025
 # File: ui/components/controls/__init__.py
 # Description: Controls package initialization
-# Created: 2025-03-16
+# Created: 2025-03-17
 
 """Control components for the MNIST Digit Classifier."""
 
 from ui.components.controls.buttons import Button, PrimaryButton, SecondaryButton, IconButton
 from ui.components.controls.bb8_toggle import BB8Toggle
+from ui.components.controls.bb8_toggle_css import get_bb8_toggle_css
+from ui.components.controls.bb8_toggle_template import get_bb8_toggle_template
 
 __all__ = [
     "Button",
     "PrimaryButton",
     "SecondaryButton",
     "IconButton",
-    "BB8Toggle"
+    "BB8Toggle",
+    "get_bb8_toggle_css",
+    "get_bb8_toggle_template"
 ]
