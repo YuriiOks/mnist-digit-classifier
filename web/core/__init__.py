3f05958602d583472807936bf56180a7efef4c74
WORKSPACE <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-24 21:50:25 +0000

    Update __init__.py

--- web/core/__init__.py
+++ web/core/__init__.py
@@ -3,11 +3,16 @@
 # File: core/__init__.py
 # Description: Core package initialization
 # Created: 2025-03-16
+# Updated: 2025-03-24
 
 """Core functionality for the MNIST Digit Classifier."""
 
-from core.app_state import SessionState, ThemeState, NavigationState, initialize_app_state
+from core.app_state.session_state import SessionState
+from core.app_state.theme_state import ThemeState
+from core.app_state.navigation_state import NavigationState
+from core.app_state import initialize_app_state
 from core.errors import ErrorHandler, UIError, TemplateError, ComponentError
+from core.database import DatabaseManager, db_manager, initialize_database
 
 __all__ = [
     "SessionState",
@@ -17,5 +22,8 @@ __all__ = [
     "ErrorHandler",
     "UIError",
     "TemplateError",
-    "ComponentError"
+    "ComponentError",
+    "DatabaseManager",
+    "db_manager",
+    "initialize_database"
 ]
