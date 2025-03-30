70e5105f6ecfa6ac31e276a13b62661007388458
WORKSPACE <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-30 00:36:53 +0000

    Update __init__.py

--- web/core/registry/__init__.py
+++ web/core/registry/__init__.py
@@ -1,12 +1,17 @@
 # MNIST Digit Classifier
-# Copyright (c) 2025
+# Copyright (c) 2025 YuriODev (YuriiOks)
 # File: core/__init__.py
 # Description: Core package initialization
 # Created: 2025-03-16
 
 """Core functionality for the MNIST Digit Classifier."""
 
-from core.app_state import SessionState, ThemeState, NavigationState, initialize_app_state
+from core.app_state import (
+    SessionState,
+    ThemeState,
+    NavigationState,
+    initialize_app_state,
+)
 from core.errors import ErrorHandler, UIError, TemplateError, ComponentError
 
 __all__ = [
@@ -17,5 +22,5 @@ __all__ = [
     "ErrorHandler",
     "UIError",
     "TemplateError",
-    "ComponentError"
+    "ComponentError",
 ]
