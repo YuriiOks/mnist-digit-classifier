01d6382986431769c1c2ff209a11521aea848b7c
WORKSPACE <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-30 00:36:23 +0000

    Update __init__.py

--- web/ui/layout/__init__.py
+++ web/ui/layout/__init__.py
@@ -1,5 +1,5 @@
 # MNIST Digit Classifier
-# Copyright (c) 2025
+# Copyright (c) 2025 YuriODev (YuriiOks)
 # File: ui/layout/__init__.py
 # Description: Layout components package initialization
 # Created: 2025-03-17
@@ -11,9 +11,4 @@ from ui.layout.header import Header
 from ui.layout.footer import Footer
 from ui.layout.sidebar import Sidebar
 
-__all__ = [
-    "Layout",
-    "Header",
-    "Footer",
-    "Sidebar"
-]
+__all__ = ["Layout", "Header", "Footer", "Sidebar"]
