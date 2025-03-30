5083e48fc95911b5064d18a17c5b97b2a9962602
WORKSPACE <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-30 19:18:02 +0100

    Reformat whitespaces

--- setup.py
+++ setup.py
@@ -10,7 +10,5 @@ from setuptools import setup, find_packages
 setup(
     name="mnist_classifier_project",  # Choose a suitable name
     version="0.1.0",
-    packages=find_packages(
-        include=["utils*", "scripts*", "model*", "web*"]
-    )
+    packages=find_packages(include=["utils*", "scripts*", "model*", "web*"]),
 )
