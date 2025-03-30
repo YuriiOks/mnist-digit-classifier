af24959ccc379c5aec910f624721772c2356e64d
WORKSPACE <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-30 00:36:47 +0000

    Update __init__.py

--- web/ui/components/__init__.py
+++ web/ui/components/__init__.py
@@ -1,5 +1,5 @@
 # MNIST Digit Classifier
-# Copyright (c) 2025
+# Copyright (c) 2025 YuriODev (YuriiOks)
 # File: ui/components/__init__.py
 # Description: Components package initialization
 # Created: 2025-03-17
@@ -8,7 +8,12 @@
 
 from ui.components.base.component import Component
 from ui.components.cards.card import Card, FeatureCard, WelcomeCard
-from ui.components.controls.buttons import Button, PrimaryButton, SecondaryButton, IconButton
+from ui.components.controls.buttons import (
+    Button,
+    PrimaryButton,
+    SecondaryButton,
+    IconButton,
+)
 from ui.components.controls.bb8_toggle import BB8Toggle
 from ui.components import navigation
 from ui.components import feedback
@@ -17,21 +22,18 @@ from ui.components import inputs
 __all__ = [
     # Base components
     "Component",
-    
     # Card components
     "Card",
     "FeatureCard",
     "WelcomeCard",
-    
     # Control components
     "Button",
     "PrimaryButton",
     "SecondaryButton",
     "IconButton",
     "BB8Toggle",
-    
     # Component packages
     "navigation",
     "feedback",
-    "inputs"
+    "inputs",
 ]
