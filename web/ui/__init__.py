34c5c690c5152f4c4ae22ba9b299619913ad5b95
WORKSPACE <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-30 00:36:48 +0000

    Update __init__.py

--- web/ui/__init__.py
+++ web/ui/__init__.py
@@ -1,5 +1,5 @@
 # MNIST Digit Classifier
-# Copyright (c) 2025
+# Copyright (c) 2025 YuriODev (YuriiOks)
 # File: ui/__init__.py
 # Description: UI package initialization
 # Created: 2025-03-17
@@ -11,26 +11,28 @@ from ui.theme.theme_manager import ThemeManager, theme_manager
 from ui.theme.css_manager import CSSManager, css_manager
 from ui.components.base.component import Component
 from ui.components.cards.card import Card, FeatureCard, WelcomeCard
-from ui.components.controls.buttons import Button, PrimaryButton, SecondaryButton, IconButton
+from ui.components.controls.buttons import (
+    Button,
+    PrimaryButton,
+    SecondaryButton,
+    IconButton,
+)
 from ui.components.controls.bb8_toggle import BB8Toggle
 from ui.views import HomeView, DrawView, HistoryView, SettingsView
 
 __all__ = [
     # Layout components
     "Layout",
-    "Header", 
+    "Header",
     "Footer",
     "Sidebar",
-    
     # Theme management
     "ThemeManager",
     "theme_manager",
     "CSSManager",
     "css_manager",
-    
     # Base components
     "Component",
-    
     # UI Components
     "Card",
     "FeatureCard",
@@ -40,10 +42,9 @@ __all__ = [
     "SecondaryButton",
     "IconButton",
     "BB8Toggle"
-
     # Views
     "HomeView",
     "DrawView",
     "HistoryView",
-    "SettingsView"
+    "SettingsView",
 ]
