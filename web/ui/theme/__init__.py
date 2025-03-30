b9a15eaa12173d6b009687c331fdeb782e7c7199
WORKSPACE <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-30 00:36:18 +0000

    Update __init__.py

--- web/ui/theme/__init__.py
+++ web/ui/theme/__init__.py
@@ -1,5 +1,5 @@
 # MNIST Digit Classifier
-# Copyright (c) 2025
+# Copyright (c) 2025 YuriODev (YuriiOks)
 # File: ui/theme/__init__.py
 # Description: Theme components package initialization
 # Created: 2025-03-16
@@ -9,9 +9,4 @@
 from ui.theme.theme_manager import ThemeManager, theme_manager
 from ui.theme.css_manager import CSSManager, css_manager
 
-__all__ = [
-    "CSSManager",
-    "css_manager",
-    "ThemeManager",
-    "theme_manager"
-]
+__all__ = ["CSSManager", "css_manager", "ThemeManager", "theme_manager"]
