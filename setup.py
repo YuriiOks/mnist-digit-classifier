cb5444a4158ead1ec520630fcc9961ebeefdca27
WORKSPACE <yurii.oksamytnyi@yuriodev.co.uk>
2025-03-30 00:37:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mnist_classifier_project", # Choose a suitable name
+    name="mnist_classifier_project",  # Choose a suitable name
     version="0.1.0",
-    packages=find_packages(include=['utils*', 'scripts*', 'model*', 'web*']), # Include your main packages
+    packages=find_packages(
+        include=["utils*", "scripts*", "model*", "web*"]
+    ),  # Include your main packages
     # You might add author, description etc. later
 )
